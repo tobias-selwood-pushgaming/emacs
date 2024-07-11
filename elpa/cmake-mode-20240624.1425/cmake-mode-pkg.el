8b90f6624310521b610d717a76c1f81b087f11fb
Tobias Selwood <Tobias.Selwood@pushgaming.com>
2024-07-11 11:00:12 +0100

    Starting setup.

---
+++ elpa/cmake-mode-20240624.1425/cmake-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "cmake-mode" "20240624.1425" "major-mode for editing CMake sources"
+  '((emacs "24.1"))
+  :commit "4b874546fc3711bb24fcdba5718e6c85a17c740a")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ transient/history.el
@@ -0,0 +1,12 @@
+((magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat")
+  nil)
+ (magit-dispatch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-push nil)
+ (magit-rebase
+  ("--autostash"))
+ (magit-revert
+  ("--edit")))
